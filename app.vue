39a976e1d06719b001c57244ab97c0039fac5216
claraa26 <clarasistaa26@gmail.com>
2025-04-14 15:13:00 +0700

    fix minor login

--- app.vue
+++ app.vue
@@ -1,19 +1,21 @@
 <script setup>
-import { onMounted } from 'vue'
 import { useRoute } from 'vue-router';
+import { computed, watchEffect } from 'vue'
 import NavbarLandingPage from './components/NavbarLandingPage.vue';
 import Navbar from './components/Navbar.vue';
 import NavbarAdmin from './components/NavbarAdmin.vue';
-import Footer from './components/Footer.vue'; // kalau belum diimport
+import Footer from './components/Footer.vue';
 
 import { useUserStore } from '@/stores/userStore'
 
-const userStore = useUserStore()
 const route = useRoute()
+const userStore = useUserStore()
+const token = localStorage.getItem('token')
 
-onMounted(() => {
-  userStore.fetchUser()
-})
+// Await fetchUser before component renders
+await userStore.fetchUser()
+
+const user = computed(() => userStore.user)
 </script>
 
 <template>
@@ -22,8 +24,8 @@ onMounted(() => {
       <NavbarLandingPage />
     </template>
     <template v-else>
-      <NavbarAdmin v-if="userStore.user && userStore.user.role_id === 1" />
-      <Navbar v-else-if="userStore.user && userStore.user.role_id === 4" />
+      <NavbarAdmin v-if="user && user.role_id === 1" />
+      <Navbar v-else-if="token && user.role_id === 4" />
     </template>
 
     <v-main>
